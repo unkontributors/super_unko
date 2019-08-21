6597d53cdc36bd809b9d7f11b3509fb95fd7f478
greymd <greengregson@gmail.com>
2019-08-21 08:05:05 +0100

    source command_not_found_handle[r] from activate.sh

--- activate.sh
+++ activate.sh
@@ -5,4 +5,6 @@ if ! type super_unko > /dev/null 2>&1; then
     cd "$(dirname "${BASH_SOURCE:-$0}")" && pwd
   )
   export PATH="$PATH:$THIS_DIR/bin"
+  # shellcheck source=/dev/null
+  source "$THIS_DIR/lib/super_unko/unko_command_not_found_hundle"
 fi
