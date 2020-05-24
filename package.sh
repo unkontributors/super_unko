6522fca96f3a0788371bc118177059bca40c75eb
greymd <yamadagrep@gmail.com>
2020-05-24 15:27:05 +0000

    Fix tar2rpm/tar2deb version 1.0.1

--- package.sh
+++ package.sh
@@ -7,9 +7,9 @@ readonly THIS_DIR
 {
   cd "${THIS_DIR}"
   tar zcvf super_unko.tar.gz bin lib .tar2package.yml
-  docker run -i greymd/tar2rpm < super_unko.tar.gz > pkg/super_unko.rpm.tmp
+  docker run -i greymd/tar2rpm:1.0.1 < super_unko.tar.gz > pkg/super_unko.rpm.tmp
   mv pkg/super_unko.rpm{.tmp,}
-  docker run -i greymd/tar2deb < super_unko.tar.gz > pkg/super_unko.deb.tmp
+  docker run -i greymd/tar2deb:1.0.1 < super_unko.tar.gz > pkg/super_unko.deb.tmp
   mv pkg/super_unko.deb{.tmp,}
   rm super_unko.tar.gz
 }
